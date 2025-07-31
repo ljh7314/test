4d0d22ea9b24ea7eca5d1020942b40dd36f251ad
ljh7314 <78801845+ljh7314@users.noreply.github.com>
2025-07-31 15:24:05 +0900

    Update rag_test3.py

--- rag_test3.py
+++ rag_test3.py
@@ -122,8 +122,8 @@ def get_text(docs):
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=900,
-        chunk_overlap=100,
+        chunk_size=200,
+        chunk_overlap=10,
         length_function=tiktoken_len
     )
     chunks = text_splitter.split_documents(text)
@@ -141,7 +141,7 @@ def get_vectorstore(text_chunks):
     return vectordb
 
 def get_conversation_chain(vetorestore):
-    llm =  ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest", temperature = 0)
+    llm =  ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite-preview-06-17", temperature = 0)
     conversation_chain = ConversationalRetrievalChain.from_llm(
             llm=llm, 
             chain_type="stuff", 
